d602b10188b9c3a9d5719a90c4bacca549fa2a35
jeff <jeff.a.nyberg@hotmail.com>
2020-12-07 22:08:54 -0600

    call mqtt to display message on index.html when loaded.

--- Mess/mqread.py
+++ Mess/mqread.py
@@ -1,14 +1,16 @@
 import paho.mqtt.client as mqtt
 
 import time
-        
+
+
+
 broker='127.0.0.1'
 client=mqtt.Client("my_mqtt")
 
 def on_connect(client, userdata,flags,rc):
     if rc==0:
-        client.connected_flag=True
-        print("connected")
+       client.connected_flag=True
+       print("connected")
     else:
         print("bad connection returned code",rc)
 
@@ -20,9 +22,26 @@ client.loop_start()
 print("connecting to server",broker)
 client.connect(broker)
 while not client.connected_flag:
-    print("in wait loop")
-    time.sleep(1)
-print("in main loop")
+     print("in wait loop")
+     time.sleep(1)
+     print("in main loop")
+client.subscribe("sensors/drone01/altitude")
+def on_message(client,userdata,msg):
+     print(msg.topic)
+     print(msg.payload)
+     A_mess = msg.payload
+     Payload(A_mess)
+              
+
+def Payload(A_mess):
+     print('in the function')
+     print(A_mess)
+     return(A_mess)
+
+client.on_message=on_message
+     
+
+time.sleep(10)
 
 client.loop_stop()
 client.disconnect()
--- Mess/views.py
+++ Mess/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from . mqtt import ReadMqtt
 
 # Create your views here.
+
+def index(request):
+    mq_mess= ''
+    mq_mess=ReadMqtt()
+    
+    print ('return from function')
+    print (mq_mess) 
+    stuff_for_frontend = {'mq_mess': mq_mess}
+    return render(request,'Mess/index.html',stuff_for_frontend)
